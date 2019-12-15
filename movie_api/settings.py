9d1ed56028a82595b9b3723519c091dbb73a5c3b
jwsong124 <tbvj011@naver.com>
2019-12-15 14:44:19 +0900

    mysql setting

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -88,9 +88,13 @@ WSGI_APPLICATION = 'movie_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'movies',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1', # Or an IP Address that your DB is hosted on
+        'PORT': '3307',
+    },
 }
 
 
